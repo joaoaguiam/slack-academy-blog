324d4e516d10ac78b76febeef78aa8b4791dd7e4
Joao Aguiam <joao@veertly.com>
2023-07-26 03:38:04 +0200

    First version of the blog

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/blog', title: 'Blog' },
+  { href: 'https://slackinaction.substack.com', title: 'Newsletter', target: '_blank' },
+  // { href: '/tags', title: 'Tags', target: '_blank' },
+  // { href: '/projects', title: 'Projects' },
+  { href: '/about', title: 'About' },
+]
+
+export default headerNavLinks
